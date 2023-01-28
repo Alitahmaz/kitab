695ad8087999831d9cd878f039a4c343b4950e49
Alitahmaz <alirt@code.edu.az>
2023-01-28 22:02:11 +0300

    first commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using bookclass;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nece kitab olacag: ");
+            int a = int.Parse(Console.ReadLine());
+            Book[] kitablar = new Book[a];
+            for ( int i=0;i<a;i++ )
+            {
+                Book b = new Book();
+                Console.Write($"{i+1}.kitabin adi: ");
+                b.name = Console.ReadLine();
+                Console.Write($"{i + 1}.muellifin adi: ");
+                b.Autname = Console.ReadLine();
+                Console.Write($"{i + 1}.nesr edilme tarixi: ");
+                b.PubDate=int.Parse(Console.ReadLine());
+                Console.Write($"{i + 1}.kitabin sehifesi: ");
+                b.PageCount=int.Parse(Console.ReadLine());
+                kitablar[i] = b;
+            }
+            foreach(var item in kitablar)
+            {
+                Console.Write(item);   
+            }
+        }
+    }
+}
